77d05460f1814fe9f7288870e586329bb6957bc5
iona.dommel-prioux <iona.dommel-prioux@donecle.com>
2023-04-11 16:16:35 +0200

    ♻️ ADDED: same route but uses temporary files

--- main.py
+++ main.py
@@ -30,3 +30,29 @@ async def process_audio(file: UploadFile = File(...)):
 
     # Return the new file to the user
     return FileResponse(new_file_path, media_type="audio/mpeg")
+
+
+@app.post("/audio")
+async def process_audio(file: UploadFile = File(...)):
+    # Create a temporary file to store the uploaded file
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_audio_file:
+        # Save the uploaded file to the temporary file
+        content = await file.read()
+        tmp_audio_file.write(content)
+
+        # Load the audio file using librosa
+        audio, sr = librosa.load(tmp_audio_file.name)
+
+        # Split the audio file in half
+        split_point = len(audio) // 2
+        audio_first_half = audio[:split_point]
+
+        # Create a temporary file to store the first half of the audio file
+        with tempfile.NamedTemporaryFile(
+            delete=False, suffix=".mp3"
+        ) as tmp_audio_half_file:
+            # Save the first half of the audio file to the temporary file
+            sf.write(tmp_audio_half_file.name, audio_first_half, sr)
+
+            # Return the new file to the user
+            return FileResponse(tmp_audio_half_file.name, media_type="audio/mpeg")
